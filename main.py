02660378fd379a5e0e2dbabf15e6799b0d9cc05e
davidgress <david.e.gress@gmail.com>
2023-09-18 07:43:30 -0400

    Day 41: Completed day 41, OOP

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ df = pandas.read_csv("hotels.csv", dtype={"id": str})
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
         """ Book a hotel by changing its availability to no"""
@@ -22,10 +23,16 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        content = f"Name of the customer hotel"
+        content = f"""
+        Thank you for your reservation!
+        Here are the details of your reservation:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
         return content
 
 
@@ -36,7 +43,7 @@ hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
     print(reservation_ticket.generate())
 else:
     print("Hotel is not free.")
